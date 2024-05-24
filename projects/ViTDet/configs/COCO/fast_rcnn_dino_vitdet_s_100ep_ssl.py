ae0c3a55d3cde62f7786312234e92b2f4e919415
Julian Lee <julianlee@linkervision.com>
2024-05-24 08:41:29 +0000

    Clean configs

--- projects/ViTDet/configs/COCO/fast_rcnn_dino_vitdet_s_100ep_ssl.py
+++ projects/ViTDet/configs/COCO/fast_rcnn_dino_vitdet_s_100ep_ssl.py
@@ -8,11 +8,13 @@ from detectron2.modeling.backbone.vit import get_vit_lr_decay_rate
 
 from ..common.coco_loader_ssl import dataloader
 
+dataloader.train.total_batch_size = 4
 
 model = model_zoo.get_config("common/models/fast_rcnn_dino_vitdet_b.py").model
 model.backbone.net.embed_dim = 384
 model.backbone.net.depth = 12
 model.backbone.net.num_heads = 6
+model.backbone.net.patch_size = 16
 
 # Initialization and trainer settings
 train = model_zoo.get_config("common/train.py").train
@@ -21,13 +23,17 @@ train.ddp.fp16_compression = True
 train.init_checkpoint = (
     "detectron2://ImageNetPretrained/MAE/mae_pretrain_vit_base.pth?matching_heuristics=True"
     # "/home/appuser/detectron2_repo/datasets/coco/dinov2_vitb14_reg4_pretrain.pth"
+    # "/home/appuser/detectron2/model-weights/dinov2_vits14_pretrain.pth"
 )
 train.output_dir = '/home/appuser/datasets/tiip/train-output/test'
-
+train.ddp.find_unused_parameters = True
 
 # Schedule
 # 100 ep = 184375 iters * 64 images/iter / 118000 images/ep
-train.max_iter = 184375
+num_epoch = 400
+num_train_images = 10209
+train.max_iter = num_epoch*num_train_images // dataloader.train.total_batch_size
+train.eval_period = num_train_images // dataloader.train.total_batch_size
 
 lr_multiplier = L(WarmupParamScheduler)(
     scheduler=L(MultiStepParamScheduler)(
